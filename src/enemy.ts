f60606a9654061215e00d34751800d92d48a51f7
marijnh01 <marijnh@hotmail.nl>
2022-06-08 11:34:41 +0200

    enemies

---
+++ src/enemy.ts
@@ -0,0 +1,39 @@
+import * as PIXI from "pixi.js"
+import { Game } from "./game"
+
+
+export class Fish extends PIXI.Sprite {
+
+    deadTexture : PIXI.Texture
+    game: Game
+    private alive : boolean = true
+    
+    constructor(game: Game,texture: PIXI.Texture, deadTexture: PIXI.Texture) {
+        super(texture)
+        this.game = game
+        this.interactive = true
+        this.buttonMode = true
+        this.deadTexture = deadTexture
+        this.on('pointerdown', () => this.killfish())
+        this.x = Math.random() * 1000
+        this.y = Math.random() * 500
+        this.scale.set(0.5 +Math.random()*1.5)
+        const myfilter = new PIXI.filters.ColorMatrixFilter()
+        this.filters = [myfilter]
+        myfilter.hue(Math.random()*360, false)
+    }
+
+    update(delta:number) {
+        if (this.alive) {
+            this.x += Math.random() * 4 - 2;
+            this.y += Math.random() * 4 - 2;
+          }
+    }
+    
+    killfish() {
+        console.log("click!")
+        this.alive = false
+        this.rotation = 0
+        this.texture = this.deadTexture
+    }
+}
--- src/game.ts
+++ src/game.ts
@@ -1,49 +1,68 @@
-import * as PIXI from "pixi.js";
-import { Player } from "./player";
+import * as PIXI from "pixi.js"
+import { Player } from "./player"
+import { Fish } from "./enemy"
+import fishImage from "./images/shark.png"
+import playerImage from "./images/spacecraft.png"
+import backgroundImage from "./images/background_nl.png"
+import deadImage from "./images/bones.png"
 
-import playerImage from "./images/spacecraft.png";
-import backgroundImage from "./images/background_nl.png";
-import { Application } from "pixi.js";
 
 
 export class Game {
 
-    pixi: PIXI.Application;
-    loader: PIXI.Loader;
-    player: Player;
-    background: PIXI.Sprite;
+    pixi: PIXI.Application
+    loader: PIXI.Loader
+    player: Player
+    background: PIXI.Sprite
+    fishes: Fish[] = []
 
-    constructor() {this.pixi = new PIXI.Application({ 
+    constructor() {
+            this.pixi = new PIXI.Application({ 
             width: screen.width,
             height: screen.height
         });
-        document.body.appendChild(this.pixi.view);
+        document.body.appendChild(this.pixi.view)
 
-        this.loader = new PIXI.Loader();
+        this.loader = new PIXI.Loader()
         this.loader
             .add("playerTexture", playerImage)
-            .add("backgroundTexture", backgroundImage);
+            .add("backgroundTexture", backgroundImage)
+            .add("fishtexture", fishImage)
+            .add("deadTexture", deadImage)
 
-        this.loader.load(() => this.doneLoading());
+        this.loader.load(() => this.doneLoading())
     }
 
 
     doneLoading() {
         //Background
         this.background = new PIXI.Sprite(this.loader.resources["backgroundTexture"].texture!);
-        this.pixi.stage.addChild(this.background);
+        this.pixi.stage.addChild(this.background)
+
+         //enemy
+            for (let i = 0; i<10; i++) {
+            console.log("spawned")
+            let fish = new Fish(this, this.loader.resources["fishTexture"].texture!, this.loader.resources["deadTexture"].texture!)
+            this.pixi.stage.addChild(fish) 
+            this.fishes.push(fish)
+           }
 
         //Player
         this.player = new Player(this, this.loader.resources["playerTexture"].texture!);
-        this.pixi.stage.addChild(this.player);
+        this.pixi.stage.addChild(this.player)
 
-        this.pixi.stage.x = this.pixi.screen.width / 2;
-        this.pixi.stage.y = this.pixi.screen.height / 2;
+        this.pixi.stage.x = this.pixi.screen.width / 2
+        this.pixi.stage.y = this.pixi.screen.height / 2
 
-        this.pixi.ticker.add((delta) => this.update(delta));
+        this.pixi.ticker.add((delta) => this.update(delta))
+
+
     }
 
     update(delta: number) {
-        this.player.update(delta);
+        this.player.update(delta)
+        for(let fish of this.fishes){
+            fish.update(delta)
+        }
     }
 }
